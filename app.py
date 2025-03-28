d29d316f0440307d87248568052a3adc69ac49e7
ELARCHAOUI <mohamedelarchaoui2003@gmail.com>
2025-03-28 01:13:57 +0100

    ModelE

--- app.py
+++ app.py
@@ -21,6 +21,9 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, message="Named tensors and all their associated APIs are an experimental feature")
+
 
 @app.route('/')
 def index():
